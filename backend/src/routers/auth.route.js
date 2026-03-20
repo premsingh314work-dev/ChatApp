a46ea0c8f0289d6db4e686cbc3c1ea4ff392bb37
Prem Singh <premsingh314.work@gmail.com>
2026-03-20 22:55:32 +0530

    Rate-limiting setuped with arcjet

--- backend/src/routers/auth.route.js
+++ backend/src/routers/auth.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { Login, Logout, Signup, UpadateUser } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/Auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const authRouter = express.Router();
 
+authRouter.use(arcjetProtection)
+
+// authRouter.get('/test',(req,res)=>{res.json({message:"Arcjet Protection working."})})
 authRouter.post('/login',Login)
 authRouter.post('/signup',Signup)
 authRouter.get('/logout',Logout)
