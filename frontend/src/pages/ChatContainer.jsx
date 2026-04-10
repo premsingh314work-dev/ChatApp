c161280b9862545ae286785a01d45b567d193aaa
Prem Singh <premsingh314.work@gmail.com>
2026-04-11 00:45:44 +0530

    add loading skeleton, message input, and no chat history placeholder to ChatContainer

--- frontend/src/pages/ChatContainer.jsx
+++ frontend/src/pages/ChatContainer.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "../components/ChatHeader";
+import NoChatHistoryPlaceholder from "../components/NoChatHistoryPlaceholder";
+import { useAuthStore } from "../store/useAuthStore.js";
+import MessagesLoadingSkeleton from "../components/MessagesLoadingSkeleton.jsx";
+import MessageInput from "../components/MessageInput.jsx";
 
 const ChatContainer = () => {
-  const { getMessagesByUserId, selectedUser, messages } = useChatStore();
-  //   console.log(selectedUser._id);
+  const { getMessagesByUserId, selectedUser, messages, isMessagesLoading } =
+    useChatStore();
+  const { authUser } = useAuthStore();
+
+  const messageEndRef = useRef(null);
   useEffect(() => {
     getMessagesByUserId(selectedUser._id);
   }, [selectedUser, getMessagesByUserId]);
@@ -12,15 +19,49 @@ const ChatContainer = () => {
   return (
     <>
       <ChatHeader />
-      {messages.length === 0 ? (
-        <div>Please Chat to see messages</div>
-      ) : (
-        <div>
-          {messages.map((message) => {
-            return <h4 key={message._id}>{message.text}</h4>;
-          })}
-        </div>
-      )}
+      <div className="flex-1 px-6 overflow-y-auto py-8">
+        {messages.length > 0 && !isMessagesLoading ? (
+          <div className="max-w-3xl mx-auto space-y-6">
+            {messages.map((msg) => (
+              <div
+                key={msg._id}
+                className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+              >
+                <div
+                  className={`chat-bubble relative ${
+                    msg.senderId === authUser._id
+                      ? "bg-cyan-600 text-white"
+                      : "bg-slate-800 text-slate-200"
+                  }`}
+                >
+                  {msg.image && (
+                    <img
+                      src={msg.image}
+                      alt="Shared"
+                      className="rounded-lg h-48 object-cover"
+                    />
+                  )}
+                  {msg.text && <p className="mt-2">{msg.text}</p>}
+                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
+                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
+              </div>
+            ))}
+            {/* 👇 scroll target */}
+            <div ref={messageEndRef} />
+          </div>
+        ) : isMessagesLoading ? (
+          <MessagesLoadingSkeleton />
+        ) : (
+          <NoChatHistoryPlaceholder name={selectedUser.fullName} />
+        )}
+      </div>
+
+      <MessageInput/>
     </>
   );
 };
