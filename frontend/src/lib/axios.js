b7dacde5406407286acac0fade86308d2ecc4c6b
Prem Singh <premsingh314.work@gmail.com>
2026-04-12 18:30:38 +0530

    fix: update base URL in axios instance for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "https://chat-app-ebon-one-41.vercel.app/api",
+      : "https://chatapp-05al.onrender.com/api",
   withCredentials: true,
 });
