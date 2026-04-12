a357f26223f4a7e2aa199d6218dfafcdabe0154a
Prem Singh <premsingh314.work@gmail.com>
2026-04-12 18:24:21 +0530

    fix: update BASE_URL for production environment in useAuthStore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { axiosInstance } from "../lib/axios";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chat-app-ebon-one-41.vercel.app/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
